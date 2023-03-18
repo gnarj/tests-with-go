19dadc8500cbe49b0373e12b01ca3c7de09fd343
RJ Deutschman <rjdeutschman21@gmail.com>
2023-03-18 09:19:30 -0700

    mocking/spies

--- countdown/countdown.go
+++ countdown/countdown.go
@@ -7,17 +7,31 @@ import (
 	"time"
 )
 
+type Sleeper interface {
+	Sleep()
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
 const finalWord = "Go!"
 const startCount = 3
 
-func Countdown(out io.Writer) {
+func Countdown(out io.Writer, s Sleeper) {
 	for i := startCount; i > 0; i-- {
 		fmt.Fprintln(out, i)
-		time.Sleep(1000 * time.Millisecond)
+		s.Sleep()
 	}
 	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &ConfigurableSleeper{5 * time.Second, time.Sleep}
+	Countdown(os.Stdout, sleeper)
 }
